b5fa6b2390204f4ac207d3a98c7dd44bc8a8bc49
alfonsoayalapaloma <alfonsoayalapaloma@gmail.com>
2023-05-31 18:27:08 -0500

    Update init.sh

--- files/init.sh
+++ files/init.sh
@@ -1,2 +1,3 @@
-[ "$(ls -A /etc/asterisk)" ] && echo "etc folder has files, not doing anything" || copy * /etc/asterisk
+[ "$(ls -A /etc/asterisk)" ] && echo "etc folder has files, not doing anything" ||  tar -xz
+f default-conf.tgz -C /etc/asterisk
 asterisk -f
